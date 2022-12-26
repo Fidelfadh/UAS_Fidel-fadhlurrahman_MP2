8fd01616276f5da9f86d8ff463693369ebdb6f3c
Fidelfadh <114160661+Fidelfadh@users.noreply.github.com>
2022-12-26 21:01:34 +0700

    Initial commit

---
+++ app/src/main/java/com/uas/fidel/fragment2.java
@@ -0,0 +1,25 @@
+package com.uas.fidel;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+public class fragment2 extends Fragment {
+    private void startSharedActivity() {
+        Intent intent = new
+                Intent(getActivity(), SharedActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment2, container, true);
+    }
+}
---
+++ app/src/main/java/com/uas/fidel/service/Service.java
@@ -0,0 +1,15 @@
+package com.uas.fidel.service;
+
+import com.uas.fidel.model.UserResponseItem;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface Service {
+
+    @GET("/users")
+    Call<List<UserResponseItem>> getDataUser();
+
+}
